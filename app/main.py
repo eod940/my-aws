b5ff634d563a45abcf6b66dd37dd85768a4202bb
eod940 <eod940@gmail.com>
2023-02-22 18:59:54 +0900

    :sparkles: (main) 기능추가: item과 해당 값을 보여주는 기능 추가

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,7 @@ app = FastAPI()
 @app.get("/")
 def root():
 	return {"Hello~~Hello~~Hello~~Hello~~": "no"}
+
+
+def item(item_id: int, q: Union[str, None] = None):
+	return {"item_id: ": item_id, "q": q}
