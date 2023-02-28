cdb5f0e687b3b9227c8744fab472e2bc009d8613
eod940 <eod940@gmail.com>
2023-02-28 21:33:05 +0900

    :hammer: (main) 기능수정: item 함수에 post, put 요청 추가

--- app/main.py
+++ app/main.py
@@ -1,14 +1,39 @@
-from typing import Union
+from typing import Union, Dict
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    user_id: str
+    password: str
+
+
 @app.get("/")
 def root():
-	return {"Hello~~Hello~~Hello~~Hello~~": "no"}
+    return {"Hello~~Hello~~Hello~~Hello~~": "no"}
 
 
-def item(item_id: int, q: Union[str, None] = None):
-	return {"item_id: ": item_id, "q": q}
+@app.get("/items/{item_id}")
+def item(item_id: int):
+    return {"item_id: ": item_id}
+
+
+@app.post("/register")
+def register_item(item: Item):
+	global dicted_item
+	dicted_item: dict[str, bool] = dict(item)
+	dicted_item['success'] = True
+	
+	return JSONResponse(dicted_item)
+
+
+@app.put("/update")
+def update_item(item: Item):
+	dicted_item = {k:v for k, v in dict(item).items()}
+	dicted_item['success'] = True
+
+	return JSONResponse(dicted_item)
